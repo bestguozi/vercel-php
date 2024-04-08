38ce397ca31b7790b450d87c781dde82a92a593a
bestguozi <mei.guozi@qq.com>
2024-04-08 14:13:58 +0800

    css

---
+++ frontend/web/assets/Math-BQ7_eNGd.js
@@ -0,0 +1,1 @@
+import{b as s,c as r,f as a}from"./index-P32P-s7l.js";const _=(t,e)=>{const c=t.__vccOpts||t;for(const[o,n]of e)c[o]=n;return c},l={},f=a("h1",null,"Math",-1),d=[f];function h(t,e){return s(),r("div",null,d)}const u=_(l,[["render",h]]);export{u as default};
